53236d50efa42f4bb499196a3975798fc065b7ad
TheFredward <fventura59@gmail.com>
2019-01-02 23:11:49 -0600

    Added fragments and edited navigation drawer

--- app/src/main/java/com/example/fredward/firstapplication/MainActivity.java
+++ app/src/main/java/com/example/fredward/firstapplication/MainActivity.java
@@ -5,7 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
@@ -22,32 +22,40 @@ public class MainActivity extends AppCompatActivity {
          /* Create a Nav drawer that will show up on app bar
         Data will be acquired from the drawer_menu.xml
         assign mDrawerLayout
-    */
+         */
          Toolbar toolbar = findViewById(R.id.toolbar);
          setSupportActionBar(toolbar);
-         ActionBar actionBar = getSupportActionBar();
-         //THis if statement handles null exception but does not solve it
-        //setDisplayAsUpenabled is invokes null pointer obhjec, need to slove
-         if(actionBar != null){
-             actionBar.setDisplayHomeAsUpEnabled(true);
-             actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
-         }
 
+         mDrawerLayout = findViewById(R.id.drawer_layout);
 
+         ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this,mDrawerLayout,toolbar,R.string.openNav,R.string.closeNav);
+         mDrawerLayout.addDrawerListener(actionBarDrawerToggle);
+         actionBarDrawerToggle.syncState();
 
-        mDrawerLayout = findViewById(R.id.drawer_layout);
-         /* I get the nav menu with the expect design
+        /**
+         * NavigationView to open the user requested menu:
+         * Using onclicklistener and switch statement to open the correct menu
+         */
 
-          */
         NavigationView navigationView = findViewById(R.id.Nav_menu);
-        //same error here nullpointer exception
-        if(navigationView != null){
             navigationView.setNavigationItemSelectedListener(
                     new NavigationView.OnNavigationItemSelectedListener() {
                         @Override
                         public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                            //Go to the selected tab by the user
+                            //using menuItem compare the selection and go to appropriate fragment
+                            switch (menuItem.getItemId()){
+                                case (R.id.recent_release):
+                                    getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,new RecentFragment()).commit();
+                                    break;
+                                case (R.id.favorites):
+                                    getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, new FavoritesFragment()).commit();
+                                    break;
+                                case (R.id.settings):
+                                    getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, new UpdateFragment()).commit();
+                                    break;
+                            }
                             menuItem.setChecked(true);
-
                             mDrawerLayout.closeDrawers();
                             return true;
                         }
@@ -56,6 +64,18 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
+    /**
+     * override onBackPressed for back button press when menu is open
+     * Close the menu instead of the app
+     */
+
+    @Override
+    public void onBackPressed() {
+        if(mDrawerLayout.isDrawerOpen(GravityCompat.START)){
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        }else {
+            super.onBackPressed();
+        }
     }
 
     @Override
---
+++ app/src/main/java/com/example/fredward/firstapplication/UpdateFragment.java
@@ -0,0 +1,18 @@
+package com.example.fredward.firstapplication;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class UpdateFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.update_layout,container,false);
+    }
+}
